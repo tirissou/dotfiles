f830face27dc990b422ae9aec432433bf8c610d3
tirissou <irissou.thibault@gmail.com>
2022-02-25 19:06:28 +0000

    build nvim from source in setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+
+# Requirements
+sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+
+# Build neovim from source
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+# Local user build
+# make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME"
+# make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME" install
+# Root build
+make
+make install
+cd ..
+rm -rf neovim
+cd
